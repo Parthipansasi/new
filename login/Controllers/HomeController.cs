2eef4169cb1cf5ca1e9e87780465c76450a9efa8
Parthipansasi <115632508+Parthipansasi@users.noreply.github.com>
2022-11-15 15:51:47 +0530

    change text

--- login/Controllers/HomeController.cs
+++ login/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace login.Controllers
         db dbop = new db();
         public ActionResult Index()
         {
-            return View();
+            return View("hi this is");
         }
         [HttpPost]
         public ActionResult Index([Bind] ad_login ad)
